07ff19b53e1e9944bcc7a8afdb7fd86eb40950c3
re757575 <re757575@gmail.com>
2015-03-13 11:40:39 +0800

    Modify:使用發佈版本

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
-
+﻿
 function onClickHandler(info, tab) {
     console.log(info);
     if (info.menuItemId === 'child1') {
 		console.log("中翻英");
 		var q = info.selectionText;
 		chrome.tabs.create({
-			"url" : "https://script.google.com/macros/s/AKfycbzNdVaoJopNtgFguRc6R3uxiQO26uK5CN0ONWOQ34g/dev?q="+q+"&source=zh-TW&target=en"
+			"url" : "https://script.google.com/macros/s/AKfycbzVbbioKRcPimW2ZGN0kAEZKgDSqV2bZ44GIr6HDvQOy4lcpeM/exec?q="+q+"&source=zh-TW&target=en"
 		});
     } else {
 		console.log("英翻中");
 		var q = info.selectionText;
 		chrome.tabs.create({
 			//"url" : "https://translate.google.com.tw/?hl=zh-TW&tab=wT#en/zh-TW/" + st
-			"url" : "https://script.google.com/macros/s/AKfycbzNdVaoJopNtgFguRc6R3uxiQO26uK5CN0ONWOQ34g/dev?q="+q+"&source=en&target=zh-TW"
+			"url" : "https://script.google.com/macros/s/AKfycbzVbbioKRcPimW2ZGN0kAEZKgDSqV2bZ44GIr6HDvQOy4lcpeM/exec?q="+q+"&source=en&target=zh-TW"
 		});
     }
 }
